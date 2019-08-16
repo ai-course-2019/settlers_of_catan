20b8089cfdc99ac6c604ccb6dc43bd427f59b0fa
Erez Levy <erezlevy93@gmail.com>
2019-08-16 15:19:33 +0300

    fixed catan_state.py method 'unmake_random_move' to match the last changes in 'make_random_move'

--- game/catan_state.py
+++ game/catan_state.py
@@ -173,7 +173,7 @@ class CatanState(AbstractState):
         self._purchased_development_cards_in_current_turn_amount = 0
 
         # Updating the current_player_index (Default - next player, Initilisation Phase - next/same/previous player).
-        if self.turns_count == len(self.players) or self.turns_count == 2 *len(self.players):
+        if self.turns_count == len(self.players) or self.turns_count == 2 * len(self.players):
             return
         elif len(self.players) < self.turns_count < 2 * len(self.players):
             self._current_player_index = (self._current_player_index - 1) % len(self.players)
@@ -181,7 +181,12 @@ class CatanState(AbstractState):
             self._current_player_index = (self._current_player_index + 1) % len(self.players)
 
     def unmake_random_move(self, random_move: RandomMove):
-        self._current_player_index = (self._current_player_index - 1) % len(self.players)
+        if self.turns_count == len(self.players) or self.turns_count == 2 * len(self.players):
+            pass
+        elif len(self.players) < self.turns_count < 2 * len(self.players):
+            self._current_player_index = (self._current_player_index + 1) % len(self.players)
+        else:
+            self._current_player_index = (self._current_player_index - 1) % len(self.players)
         random_move.revert()
 
     def get_current_player(self):
